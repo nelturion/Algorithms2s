ab4956b7774bf33d72cb7fddbe86e0619942f783
Konstantin Gritzkevich <kgritz@yandex.ru>
2025-03-20 04:22:59 +0300

    Lab 2  task 16

--- lab2/task16_not_complete/src/__init__.py
+++ lab2/task16_not_complete/src/__init__.py
@@ -1,37 +1,49 @@
-# Кастомное чтение файла для этой задачи
-def file_read(path):
-    list:
-    with open(path) as file:
-        n = file.readline()
-        for i in range(0, n):
-            action, number = file.readline().split(" ")
-            print(action, number)
-            if action == "+1" or action == "1":
+# # Кастомное чтение файла для этой задачи
+# def file_read(path):
+#     a = []
+#     with open(path) as file:
+#         n = file.readline()
+#         for i in range(0, n):
+#             action, number = file.readline().split(" ")
+#             print(action, number)
+#             if action == "+1" or action == "1":
+#                 a.append(["+", number])
+#             elif action == "0":
+#                 a.append(["=", number])
+#             else:
+#                 a.append(["-", number])
+#         file.close()
+#     return n, a
 
-    file.close()
 
-    return n
+def read_list(path_in, path_out):
+    sorted_storage = []
+    with open(path_in) as file_in:
+        n = int(file_in.readline())
+        with open(path_out, "w") as file_out:
+            for i in range(0, n):
+                action, number = file_in.readline().split(" ")
+                number = int(number)
+                print(action, number)
+                if action == "+1" or action == "1":
+                    sorted_storage.append(number)
+                    sorted_storage.sort(reverse=True)
+                elif action == "0":
+                    print(sorted_storage, sorted_storage[number-1])
+                    file_out.write(str(sorted_storage[number-1]) + "\n")
+                else:
+                    sorted_storage.remove(number)
+            file_out.close()
+        file_in.close()
 
 
 def main():
-    n, m, lead, a, b = file_read("../txtf/input.txt")
-    # print(n, m, lead, a, b)
-
-    if int(9) in b.get(lead):
-        print("У противника есть коз. туз\n")
-        output = ["NO"]
-    else:
-        print("У противника нет коз. туза\n")
-        output = check(n, m, lead, clear_dict(a), clear_dict(b))
-
-    print(f"{output}\n")
-    write_file("../txtf/output.txt", output)
+    read_list("../txtf/input.txt", "../txtf/output.txt")
 
 
 if __name__ == "__main__":
     import time
     import tracemalloc
-    from lab1.defs import *
 
     t_start = time.perf_counter()
     tracemalloc.start()
