13537a5f9a0d0943199769231397dc821b1f1810
nelturion <aandreyuk2001@gmail.com>
2025-04-01 23:03:00 +0300

    [fix]: readable comments, fixed input2

--- lab3/task14/src/__init__.py
+++ lab3/task14/src/__init__.py
@@ -16,43 +16,10 @@ def fastest_route(n, start, finish, routes):
             return curr_time
 
         for dep_time, dst, arr_time in graph[curr_village]:
-            if dep_time >= curr_time and arr_time < min_time[dst]:  # если не опоздали на абобус и время его прибытия в пункт назначения меньше минимального вермени из всех возможных временей прибыытия в этот пункт
-                min_time[dst] = arr_time
-                heapq.heappush(pq, (arr_time, dst))
+            if dep_time >= curr_time and arr_time < min_time[dst]:  # если не опоздали на абобус и время его прибытия в
+                min_time[dst] = arr_time                            # пункт назначения меньше минимального вермени из
+                heapq.heappush(pq, (arr_time, dst))                 # всех возможных временей прибытия в этот пункт
     return -1
-        # if curr_time < min_time[curr_village]:
-        #     min_time[curr_village] = curr_time
-        #     heapq.heappush(pq, (curr_time, curr_village))
-
-    # # Граф для хранения автобусных рейсов
-    # graph = {i: [] for i in range(1, n + 1)}
-
-
-#
-# for i in range(3, 3 + r):
-#     u, t1, t2, w = map(int, lines[i].split())
-#     graph[u].append((t1, t2, w))
-#
-# # Алгоритм Дейкстры с учетом времени отправления и прибытия
-# INF = float('inf')
-# min_time = [INF] * (n + 1)
-# min_time[d] = 0
-# pq = [(0, d)]  # Очередь с приоритетом (текущее время, текущая деревня)
-#
-# while pq:
-#     current_time, current_village = heapq.heappop(pq)
-#
-#     if current_time > min_time[current_village]:
-#         continue
-#
-#     for t1, t2, w in graph[current_village]:
-#         if current_time <= t1:  # Можно успеть на автобус
-#             arrival_time = t2 + w
-#             if arrival_time < min_time[w]:
-#                 min_time[w] = arrival_time
-#                 heapq.heappush(pq, (arrival_time, w))
-#
-# return min_time[d]
 
 
 if __name__ == '__main__':
@@ -62,9 +29,9 @@ if __name__ == '__main__':
     with open("../txtf/"+input_filename, 'r') as f:
         lines = f.readlines()
 
-    n = int(lines[0])  # Количество деревень
-    d, v = map(int, lines[1].split())  # Начальная и конечная деревня
-    r = int(lines[2])  # Количество автобусных рейсов
+    n = int(lines[0])                   # Количество деревень
+    d, v = map(int, lines[1].split())   # Начальная и конечная деревня
+    r = int(lines[2])                   # Количество автобусных рейсов
 
     routes = [tuple(map(int, line.strip().split())) for line in lines[3:3 + r]]  # Маршруты автобусов
 
@@ -77,3 +44,7 @@ if __name__ == '__main__':
             f.write('-1\n')  # Невозможно добраться
         else:
             f.write(str(min_time))
+
+
+### ACMP stats (comma separated):
+### id,23142393,01.04.2025,22:58:32,Андреюк Николай Ростиславович,0134,Python,Accepted,0.062,3138 Кб
