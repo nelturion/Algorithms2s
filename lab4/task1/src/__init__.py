6d95fa0580d18798b712ea740d42f7e6877ade61
nelturion <aandreyuk2001@gmail.com>
2025-04-03 12:06:19 +0300

    [feat]: task1 solution

---
+++ lab4/task1/src/__init__.py
@@ -0,0 +1,33 @@
+def naive_substring_search(pattern, text):
+    occurrences = []
+    for i in range(len(text) - len(pattern) + 1):
+        if text[i : (i + len(pattern))] == pattern:
+            occurrences.append(i+1)
+    return " ".join(map(str, occurrences))
+
+
+def custom_read(path):
+    with open(path, "r") as f:
+        p = f.readline().strip()
+        t = f.readline().strip()
+    return p, t
+
+
+def custom_write(path, text):
+    with open(path, "w") as f:
+        f.write(text)
+
+
+def data_validation(pattern, text):
+    if len(pattern) < 1:
+        return False
+    if len(text) > 10 ** 4:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    p, t = custom_read("../txtf/input.txt")
+    data_validation(p, t)
+    res = naive_substring_search(p, t)
+    custom_write("../txtf/output.txt", str(str(len(res)) + "\n" + str(res)))
