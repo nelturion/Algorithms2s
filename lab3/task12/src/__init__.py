6564d302e9aa60b30c619699f0f5a4a4847e8e94
Kostyamops <465656@niuitmo.ru>
2025-04-03 13:47:18 +0300

    Lab 3 fix

--- lab3/task12/src/__init__.py
+++ lab3/task12/src/__init__.py
@@ -3,19 +3,24 @@ def custom_read(input_file):
         lines = file_in.read().splitlines()
 
     n, m = map(int, lines[0].split())
+    print(f"Комнат: {n}\nКоридоров: {m}")
 
     graph = {}
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         graph[i] = []
 
-    for i in range(1, m + 1):
-        start_room, next_room, color = map(int, lines[i].split())
-        graph[start_room].append((next_room, color))
-        graph[next_room].append((start_room, color))
+    for i in range(1, m+1):
+        u, v, c = map(int, lines[i].split())
+        graph[u].append((v, c))
+        graph[v].append((u, c))
 
-    colors = int(lines[m + 1])
-    path = list(map(int, lines[m + 2].split())) if colors > 0 else []
+    colors = int(lines[m+1])
+    if colors > 0:
+        path = list(map(int, lines[m+2].split()))
+    else:
+        path = []
 
+    print(f"Граф: {graph}\nМаршрут: {path}")
     return graph, path
 
 
@@ -25,7 +30,7 @@ def custom_write(output_file, result):
         file_out.close()
 
 
-def read_maze(input_file, output_file):
+def check_maze(input_file, output_file):
     graph, path = custom_read(input_file)
     result = follow_path(graph, path)
     custom_write(output_file, result)
@@ -34,17 +39,23 @@ def read_maze(input_file, output_file):
 def follow_path(graph, path):
     current_room = 1
     for color in path:
-        possible_paths = [next_room for next_room, path_color in graph[current_room] if path_color == color]
+        possible_paths = []
+        for next_room, path_color in graph[current_room]:
+            if path_color == color:
+                possible_paths.append(next_room)
         if possible_paths:
-            current_room = possible_paths[0]
+            next_room = possible_paths[0]
+            print(f"Переходим из {current_room} в {next_room} | Цвет {color}")
+            current_room = next_room
         else:
+            print(f"Осуществить переход невозможно")
             return "INCORRECT"
 
     return str(current_room)
 
 
 def main():
-    read_maze("../txtf/input.txt", "../txtf/output.txt")
+    check_maze("../txtf/input.txt", "../txtf/output.txt")
 
 
 if __name__ == "__main__":
